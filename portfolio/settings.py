b4ea969d7e196c7bdf394468b33b2f852188fbf9
Gustavo Maciel <gm.nunes92@gmail.com>
2018-06-04 10:25:17 -0300

    settings

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -115,7 +115,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
-STATIC_URL = '/static/portfolio/'
+STATIC_URL = '/static/'
 LOGIN_URL = '/login/'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
